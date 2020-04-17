738c3533a43218c2a8852014f8a7a629780fe93d
Alfinity <its.alfinity@gmail.com>
2020-04-17 15:18:30 +0300

    Added requests filter by origin

--- source/server.js
+++ source/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
+app.disable('x-powered-by');
 
 const routes = require('./routes.js');
-
-const API_VERISON = 'v1';
 const PORT = process.env.PORT || 8080;
 
-app.use(`/api/${API_VERISON}`, routes);
+app.use((req, res, next) => {
+  if (req.headers.origin === 'http://localhost:1337') {
+    res.set('Access-Control-Allow-Origin', req.headers.origin);
+    next();
+  }
+  res.status(403).send();
+});
+
+app.use(`/api`, routes);
 
 app.listen(PORT, () => {
   console.log(`Express listening on port ${PORT}`);
